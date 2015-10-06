61aa86629e806e2e51ff0a04af7f10ff13a6372f
Romain Derocle <rderocle@gmail.com>
2015-10-06 21:55:35 +0200

    Update seloger-gauge-fixer.user.js
    Do not run scripts if gauge is not in the page.
    Add auto update url.

--- seloger-gauge-fixer.user.js
+++ seloger-gauge-fixer.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name         SeLoger Price Gauge Corrector
-// @version      0.1
+// @version      0.2
 // @description  Correct wrong gauge indicator
 // @author       Romain Derocle <rderocle@gmail.com>
 // @match        http://www.seloger.com/*
 // @grant        none
+// @updateURL    https://github.com/monsieurchico/gm-scripts/raw/gh-pages/seloger-gauge-fixer.user.js
+
 // ==/UserScript==
 
-var surface = parseFloat($('input[name="prixImmo-surface"]').val().replace(' ', ''));
-var price = parseFloat($('input[name="prixImmo-bien"]').val().replace(' ', ''));
-var min = parseFloat($('input[name="prixImmo-min"]').val().replace(' ', ''));
-var max = parseFloat($('input[name="prixImmo-max"]').val().replace(' ', ''));
+if ($('.prixImmo-fourchette').length > 0) {
+    var surface = parseFloat($('input[name="prixImmo-surface"]').val().replace(' ', ''));
+    var price = parseFloat($('input[name="prixImmo-bien"]').val().replace(' ', ''));
+    var min = parseFloat($('input[name="prixImmo-min"]').val().replace(' ', ''));
+    var max = parseFloat($('input[name="prixImmo-max"]').val().replace(' ', ''));
 
-var percent = ((price / surface) - min) / (max - min) * 100;
-$('.prixImmo-bien').css('width', percent + '%');
+    var percent = ((price / surface) - min) / (max - min) * 100;
+    $('.prixImmo-bien').css('width', percent + '%');
+}
